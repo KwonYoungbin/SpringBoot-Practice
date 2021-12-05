a7ca4e60b8d21b87eda1c075e5c7d0bc7d127104
Kwon Youngbin <kyb951020@naver.com>
2021-12-05 21:33:40 +0900

    SpringBoot 자동설정 (AutoConfiguration) 이해

--- Chapter02/src/main/java/com/springbootTest/Chapter02Application.java
+++ Chapter02/src/main/java/com/springbootTest/Chapter02Application.java
@@ -1,9 +1,15 @@
 package com.springbootTest;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+//@SpringBootConfiguration		@SpringBootApplication에 포함된 주요 기능 3가지
+//@ComponentScan
+//@EnableAutoConfiguration
 public class Chapter02Application {
 
 	public static void main(String[] args) {
