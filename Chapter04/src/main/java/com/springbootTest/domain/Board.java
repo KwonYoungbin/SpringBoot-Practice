d858c2ac572b4d1f07e39ae57378824a2b031368
Kwon Youngbin <kyb951020@naver.com>
2021-12-29 14:32:51 +0900

    JPA 설정/ 테이블 전략 사용

--- Chapter04/src/main/java/com/springbootTest/domain/Board.java
+++ Chapter04/src/main/java/com/springbootTest/domain/Board.java
@@ -2,23 +2,20 @@ package com.springbootTest.domain;
 
 import java.io.Serializable;
 import java.util.Date;
-
 import javax.persistence.*;
 
-/**
- * Entity implementation class for Entity: Board
- *
- */
 @Entity
-@Table(name = "Board")
-public class Board implements Serializable {
-	//	private static final long serialVersionUID = 1L;
+@TableGenerator(name = "BOARD_SEQ_GENERATOR", table = "ALL_SEQUENCES",
+			pkColumnValue = "BOARD_SEQ", initialValue = 0, allocationSize = 1)
+public class Board{
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy = GenerationType.TABLE, generator = "BOARD_SEQ_GENERATOR")
 	private Long seq;
+	
 	private String title;
 	private String writer;
 	private String content;
+	@Temporal(TemporalType.DATE)
 	private Date createDate;
 	private Long cnt;
 	
@@ -75,10 +72,73 @@ public class Board implements Serializable {
 		return "Board [seq=" + seq + ", title=" + title + ", writer=" + writer + ", content="
 				+ content + ", createDate=" + createDate + ", cnt=" + cnt + "]";
 	}
-	
-	
-	
-	
-
-   
 }
+
+
+//@Entity
+//@Table(name = "Board")
+//public class Board implements Serializable {
+//	//	private static final long serialVersionUID = 1L;
+//	@Id
+//	@GeneratedValue
+//	private Long seq;
+//	private String title;
+//	private String writer;
+//	private String content;
+//	private Date createDate;
+//	private Long cnt;
+//	
+//	public Long getSeq() {
+//		return seq;
+//	}
+//
+//	public void setSeq(Long seq) {
+//		this.seq = seq;
+//	}
+//
+//	public String getTitle() {
+//		return title;
+//	}
+//
+//	public void setTitle(String title) {
+//		this.title = title;
+//	}
+//
+//	public String getWriter() {
+//		return writer;
+//	}
+//
+//	public void setWriter(String writer) {
+//		this.writer = writer;
+//	}
+//
+//	public String getContent() {
+//		return content;
+//	}
+//
+//	public void setContent(String content) {
+//		this.content = content;
+//	}
+//
+//	public Date getCreateDate() {
+//		return createDate;
+//	}
+//
+//	public void setCreateDate(Date createDate) {
+//		this.createDate = createDate;
+//	}
+//
+//	public Long getCnt() {
+//		return cnt;
+//	}
+//
+//	public void setCnt(Long cnt) {
+//		this.cnt = cnt;
+//	}
+//
+//	@Override
+//	public String toString() {
+//		return "Board [seq=" + seq + ", title=" + title + ", writer=" + writer + ", content="
+//				+ content + ", createDate=" + createDate + ", cnt=" + cnt + "]";
+//	}
+//}
