0c7cfd56eb19e66365991032eb6791a901ad0c55
Kwon Youngbin <kyb951020@naver.com>
2021-12-29 14:40:36 +0900

    JPA 설정 / TABLE, SEQUENCE, AUTO 전략 설정 방법

--- Chapter04/src/main/java/com/springbootTest/domain/Board.java
+++ Chapter04/src/main/java/com/springbootTest/domain/Board.java
@@ -4,18 +4,17 @@ import java.io.Serializable;
 import java.util.Date;
 import javax.persistence.*;
 
+//------------------- AUTO 전략 사용 -----------------------
 @Entity
-@TableGenerator(name = "BOARD_SEQ_GENERATOR", table = "ALL_SEQUENCES",
-			pkColumnValue = "BOARD_SEQ", initialValue = 0, allocationSize = 1)
-public class Board{
+@Table(name = "Board")
+public class Board implements Serializable {
+	//	private static final long serialVersionUID = 1L;
 	@Id
-	@GeneratedValue(strategy = GenerationType.TABLE, generator = "BOARD_SEQ_GENERATOR")
+	@GeneratedValue
 	private Long seq;
-	
 	private String title;
 	private String writer;
 	private String content;
-	@Temporal(TemporalType.DATE)
 	private Date createDate;
 	private Long cnt;
 	
@@ -73,18 +72,23 @@ public class Board{
 				+ content + ", createDate=" + createDate + ", cnt=" + cnt + "]";
 	}
 }
+//-----------------------------------------------------------------
 
-
+//------------------- SEQUENCE 전략 사용 -----------------------
 //@Entity
-//@Table(name = "Board")
-//public class Board implements Serializable {
-//	//	private static final long serialVersionUID = 1L;
+//@SequenceGenerator(name = "BOARD_SEQ_GENERATOR",
+//			sequenceName = "BOARD_SEQUENCE",
+//			initialValue = 1,
+//			allocationSize = 1)		
+//public class Board{
 //	@Id
-//	@GeneratedValue
+//	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "BOARD_SEQ_GENERATOR")
 //	private Long seq;
+//	
 //	private String title;
 //	private String writer;
 //	private String content;
+//	@Temporal(TemporalType.DATE)
 //	private Date createDate;
 //	private Long cnt;
 //	
@@ -142,3 +146,76 @@ public class Board{
 //				+ content + ", createDate=" + createDate + ", cnt=" + cnt + "]";
 //	}
 //}
+//-----------------------------------------------------------------
+
+// ------------------- TABLE 전략 사용 -----------------------
+//@Entity
+//@TableGenerator(name = "BOARD_SEQ_GENERATOR", table = "ALL_SEQUENCES",
+//			pkColumnValue = "BOARD_SEQ", initialValue = 0, allocationSize = 1)
+//public class Board{
+//	@Id
+//	@GeneratedValue(strategy = GenerationType.TABLE, generator = "BOARD_SEQ_GENERATOR")
+//	private Long seq;
+//	
+//	private String title;
+//	private String writer;
+//	private String content;
+//	@Temporal(TemporalType.DATE)
+//	private Date createDate;
+//	private Long cnt;
+//	
+//	public Long getSeq() {
+//		return seq;
+//	}
+//
+//	public void setSeq(Long seq) {
+//		this.seq = seq;
+//	}
+//
+//	public String getTitle() {
+//		return title;
+//	}
+//
+//	public void setTitle(String title) {
+//		this.title = title;
+//	}
+//
+//	public String getWriter() {
+//		return writer;
+//	}
+//
+//	public void setWriter(String writer) {
+//		this.writer = writer;
+//	}
+//
+//	public String getContent() {
+//		return content;
+//	}
+//
+//	public void setContent(String content) {
+//		this.content = content;
+//	}
+//
+//	public Date getCreateDate() {
+//		return createDate;
+//	}
+//
+//	public void setCreateDate(Date createDate) {
+//		this.createDate = createDate;
+//	}
+//
+//	public Long getCnt() {
+//		return cnt;
+//	}
+//
+//	public void setCnt(Long cnt) {
+//		this.cnt = cnt;
+//	}
+//
+//	@Override
+//	public String toString() {
+//		return "Board [seq=" + seq + ", title=" + title + ", writer=" + writer + ", content="
+//				+ content + ", createDate=" + createDate + ", cnt=" + cnt + "]";
+//	}
+//}
+//-------------------------------------------------------------------
