a0d9d25af6193f4be91657ca5b24db5838d22bf7
Kwon Youngbin <kyb951020@naver.com>
2021-12-30 13:18:38 +0900

    JPA API와 영속성 컨텍스트(Persistence Context)의 이해 및 엔티티 상태 이해, 삽입.수정.삭제.조회 방법학습

--- Chapter04/src/main/java/com/springbootTest/JPAClient.java
+++ Chapter04/src/main/java/com/springbootTest/JPAClient.java
@@ -1,6 +1,7 @@
 package com.springbootTest;
 
 import java.util.Date;
+import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -14,7 +15,7 @@ public class JPAClient {
 		// EntityManager 생성
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("Chapter04");
 		EntityManager em = emf.createEntityManager();
-		//----------------------- 테이블 생성 및 트랜잭션(INSERT) 수행 -----------------------
+		//----------------------- 목록 검색과 JPQL -----------------------
 		// Transaction 생성
 		EntityTransaction tx = em.getTransaction();
 		try {
@@ -28,28 +29,139 @@ public class JPAClient {
 			board.setCreateDate(new Date());
 			board.setCnt(0L);
 			
+			Board board2 = new Board();
+			board2.setTitle("JPA 제목2");
+			board2.setWriter("관리자2");
+			board2.setContent("JPA 글 등록 잘 되네요.2");
+			board2.setCreateDate(new Date());
+			board2.setCnt(1L);
+			
 			// 글 등록
 			em.persist(board);
-			
+			em.persist(board2);
+						
 			// Transaction commit
 			tx.commit();
+			
+			String jpql = "select b from Board b order by b.seq desc";
+			List<Board> boardList = em.createQuery(jpql, Board.class).getResultList();
+			
+			for (Board brd : boardList) {
+				System.out.println("---> " + brd.toString());
+			}
+			
+			tx.commit();
+			
 		} catch(Exception e) {
 			e.printStackTrace();
 			tx.rollback();
-		}
-		//-----------------------------------------------------------------------------
-		
-		//----------------------- 테이블의 검색 기능 -----------------------
-		try {
-			Board searchBoard = em.find(Board.class, 1L);
-			System.out.println("---> " + searchBoard.toString());
-			
-		} catch (Exception e) {
-			e.printStackTrace();
 		} finally {
 			em.close();
 			emf.close();
 		}
-		//-------------------------------------------------------------
+		//-----------------------------------------------------------------------------
 	}
+	
+	
+//	public static void main(String[] args) {
+//		// EntityManager 생성
+//		EntityManagerFactory emf = Persistence.createEntityManagerFactory("Chapter04");
+//		EntityManager em = emf.createEntityManager();
+//		//----------------------- 엔티티 삭제 -----------------------
+//		// Transaction 생성
+//		EntityTransaction tx = em.getTransaction();
+//		try {
+//			// Transaction 시작
+//			tx.begin();
+//			
+//			// 삭제할 게시글 조회
+//			Board board1 = em.find(Board.class, 1L);
+//			board1.setSeq(1L);
+//			
+//			// 게시글 삭제
+//			em.remove(board1);
+//						
+//			// Transaction commit
+//			tx.commit();
+//		} catch(Exception e) {
+//			e.printStackTrace();
+//			tx.rollback();
+//		} finally {
+//			em.close();
+//			emf.close();
+//		}
+//		//-----------------------------------------------------------------------------
+//	}
+	
+	
+//	public static void main(String[] args) {
+//		// EntityManager 생성
+//		EntityManagerFactory emf = Persistence.createEntityManagerFactory("Chapter04");
+//		EntityManager em = emf.createEntityManager();
+//		//----------------------- 엔티티 수정 -----------------------
+//		// Transaction 생성
+//		EntityTransaction tx = em.getTransaction();
+//		try {
+//			// Transaction 시작
+//			tx.begin();
+//			
+//			Board board = em.find(Board.class, 1L);
+//			board.setTitle("제목 수정 테스트");
+//			
+//						
+//			// Transaction commit
+//			tx.commit();
+//		} catch(Exception e) {
+//			e.printStackTrace();
+//			tx.rollback();
+//		} finally {
+//			em.close();
+//			emf.close();
+//		}
+//		//-----------------------------------------------------------------------------
+//	}
+		
+	
+//	public static void main(String[] args) {
+//		// EntityManager 생성
+//		EntityManagerFactory emf = Persistence.createEntityManagerFactory("Chapter04");
+//		EntityManager em = emf.createEntityManager();
+//		//----------------------- 테이블 생성 및 트랜잭션(INSERT) 수행 -----------------------
+//		// Transaction 생성
+//		EntityTransaction tx = em.getTransaction();
+//		try {
+//			// Transaction 시작
+//			tx.begin();
+//			
+//			Board board = new Board();
+//			board.setTitle("JPA 제목");
+//			board.setWriter("관리자");
+//			board.setContent("JPA 글 등록 잘 되네요.");
+//			board.setCreateDate(new Date());
+//			board.setCnt(0L);
+//			
+//			// 글 등록
+//			em.persist(board);
+//			
+//			// Transaction commit
+//			tx.commit();
+//		} catch(Exception e) {
+//			e.printStackTrace();
+//			tx.rollback();
+//		}
+//		//-----------------------------------------------------------------------------
+//		
+//		//----------------------- 테이블의 검색 기능 -----------------------
+//		try {
+//			Board searchBoard = em.find(Board.class, 1L);
+//			System.out.println("---> " + searchBoard.toString());
+//			
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		} finally {
+//			em.close();
+//			emf.close();
+//		}
+//		//-------------------------------------------------------------
+//	}
 }
