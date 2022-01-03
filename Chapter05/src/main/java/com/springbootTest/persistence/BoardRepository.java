eba1203794c1ebb821ebbf01fd1e846778eac6e5
Kwon Youngbin <kyb951020@naver.com>
2022-01-03 14:12:44 +0900

    페이징과 정렬 처리 방법

--- Chapter05/src/main/java/com/springbootTest/persistence/BoardRepository.java
+++ Chapter05/src/main/java/com/springbootTest/persistence/BoardRepository.java
@@ -2,12 +2,24 @@ package com.springbootTest.persistence;
 
 import java.util.List;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 
 import com.springbootTest.domain.Board;
 
 public interface BoardRepository extends CrudRepository<Board, Long> {
+	// 기본형 쿼리 메세지
 	List<Board> findByTitle(String searchKeyword);
+	
+	// SQL의 LIKE 연산자 사용
 	List<Board> findByContentContaining(String searchKeyword);
+	
+	// 여러 조건을 사용하는 WHERE절
 	List<Board> findByTitleContainingOrContentContaining(String title, String content);
+	
+	// 데이터 정렬하기
+	List<Board> findByTitleContainingOrderBySeqDesc(String searchKeyword);
+	
+	// 페이징 처리
+	List<Board> findByTitleContaining(String searchKeyword, Pageable paging);
 }
--- Chapter05/src/test/java/com/springbootTest/QueryMethodTest.java
+++ Chapter05/src/test/java/com/springbootTest/QueryMethodTest.java
@@ -6,6 +6,9 @@ import java.util.List;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 
 import com.springbootTest.domain.Board;
 import com.springbootTest.persistence.BoardRepository;
@@ -28,7 +31,7 @@ public class QueryMethodTest {
 //		}
 //	}
 //	
-	// 기본 쿼리 메세지
+
 //	@Test
 //	public void testFindByTitle() {
 //		List<Board> boardList = boardRepo.findByTitle("테스트 제목 10");
@@ -39,7 +42,6 @@ public class QueryMethodTest {
 //		}
 //	}
 	
-	// SQL의 LIKE 연산자 사용
 //	@Test
 //	public void testByContentContaning() {
 //		List<Board> boardList = boardRepo.findByContentContaining("17");
@@ -50,14 +52,47 @@ public class QueryMethodTest {
 //		}
 //	}
 	
-	// 여러 조건을 사용하는 WHERE절
+//	@Test
+//	public void testFindByTitleContainingOrContentContaining() {
+//		List<Board> boardList = boardRepo.findByTitleContainingOrContentContaining("17", "17");
+//		
+//		System.out.println("검색 결과");
+//		for (Board board : boardList) {
+//			System.out.println("---> " + board.toString());
+//		}		
+//	}
+	
+//	@Test
+//	public void testFindByTitleContainingOrderBySeqDesc() {
+//		List<Board> boardList = boardRepo.findByTitleContainingOrderBySeqDesc("17");
+//		
+//		System.out.println("검색 결과");
+//		for (Board board : boardList) {
+//			System.out.println("---> " + board.toString());
+//		}
+//	}
+	
+	// 페이징만 처리하는 방법
+//	@Test
+//	public void testFindByTitleContaining() {
+//		Pageable paging = PageRequest.of(0, 5);
+//		List<Board> boardList = boardRepo.findByTitleContaining("제목", paging);
+//		
+//		System.out.println("검색 결과");
+//		for (Board board : boardList) {
+//			System.out.println("---> " + board.toString());
+//		}
+//	}
+	
+	// 페이징 처리와 함께 데이터 정렬해서 출력하는 방법
 	@Test
-	public void testFindByTitleContainingOrContentContaining() {
-		List<Board> boardList = boardRepo.findByTitleContainingOrContentContaining("17", "17");
+	public void testFindByTitleContaining() {
+		Pageable paging = PageRequest.of(0, 5, Sort.Direction.DESC, "seq");
+		List<Board> boardList = boardRepo.findByTitleContaining("제목", paging);
 		
 		System.out.println("검색 결과");
 		for (Board board : boardList) {
 			System.out.println("---> " + board.toString());
-		}		
+		}
 	}
 }
