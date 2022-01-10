6b292ff715bf15415dafb5d152312766afb3d19d
Kwon Youngbin <kyb951020@naver.com>
2022-01-10 16:19:15 +0900

    시큐리티 적용 방법 학습

---
+++ Chapter07/src/main/java/com/springbootTest/Chapter07Application.java
@@ -0,0 +1,13 @@
+package com.springbootTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chapter07Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Chapter07Application.class, args);
+	}
+
+}
---
+++ Chapter07/src/test/java/com/springbootTest/Chapter07ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chapter07ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
