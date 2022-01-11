ba9fc57044f5838a230999491ae6e1cafd1cf115
Kwon Youngbin <kyb951020@naver.com>
2022-01-11 16:24:54 +0900

    Role을 Enum 타입으로 처리 후, JPA 연동을 위한 엔티티 클래스와 리포지터리 작성

---
+++ Chapter07/src/main/java/com/springbootTest/domain/Role.java
@@ -0,0 +1,5 @@
+package com.springbootTest.domain;
+
+public enum Role {
+	ROLE_ADMIN, ROLE_MANAGER, ROLE_MEMBER
+}
---
+++ Chapter07/src/main/java/com/springbootTest/persistence/MemberRepository.java
@@ -0,0 +1,9 @@
+package com.springbootTest.persistence;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.springbootTest.domain.Member;
+
+public interface MemberRepository extends CrudRepository<Member, String>{
+
+}
