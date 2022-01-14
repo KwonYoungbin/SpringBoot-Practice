25d7df4de72640e6f04bbdb71ddaa4eb10ede154
Kwon Youngbin <kyb951020@naver.com>
2022-01-14 14:27:00 +0900

    게시판 컨트롤러 작성

---
+++ BoardWeb/src/main/java/com/yb/board/controller/BoardController.java
@@ -0,0 +1,24 @@
+package com.yb.board.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.yb.board.domain.Board;
+import com.yb.board.service.BoardService;
+
+@Controller
+@RequestMapping("/board/")
+public class BoardController {
+	@Autowired
+	private BoardService boardService;
+	
+	@RequestMapping("/getBoardList")
+	public String getBoardList(Model model, Board board) {
+		Page<Board> boardList = boardService.getBoardList(board);
+		model.addAttribute("boardList", boardList);
+		return "board/getBoardList";
+	}
+}
