ba4ce5b05e2bfd3fc85a3c970f128ef75f3a52cb
Kwon Youngbin <kyb951020@naver.com>
2022-01-14 15:00:01 +0900

    글 등록 기능 구현

--- BoardWeb/src/main/java/com/yb/board/controller/BoardController.java
+++ BoardWeb/src/main/java/com/yb/board/controller/BoardController.java
@@ -5,6 +5,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.yb.board.domain.Board;
@@ -27,5 +28,16 @@ public class BoardController {
 	public String getBoard(Board board, Model model) {
 		model.addAttribute("board", boardService.getBoard(board));
 		return "board/getBoard";
+	}
+	
+	@GetMapping("/insertBoard")
+	public String insertBoardView() {
+		return "board/insertBoard";
+	}
+	
+	@PostMapping("/insertBoard")
+	public String insertBoard(Board board) {
+		boardService.insertBoard(board);
+		return "redirect:getBoardList";
 	}
 }
