21af6e72249311d4e01b2533dae681aea43a21af
Kwon Youngbin <kyb951020@naver.com>
2022-01-14 15:11:45 +0900

    게시글 수정 및 삭제 기능 구현

--- BoardWeb/src/main/java/com/yb/board/controller/BoardController.java
+++ BoardWeb/src/main/java/com/yb/board/controller/BoardController.java
@@ -40,4 +40,16 @@ public class BoardController {
 		boardService.insertBoard(board);
 		return "redirect:getBoardList";
 	}
+	
+	@PostMapping("/updateBoard")
+	public String updateBoard(Board board) {
+		boardService.updateBoard(board);
+		return "forward:getBoardList";
+	}
+	
+	@GetMapping("/deleteBoard")
+	public String deleteBoard(Board board) {
+		boardService.deleteBoard(board);
+		return "forward:getBoardList";
+	}
 }
