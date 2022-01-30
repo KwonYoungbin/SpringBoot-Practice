12c21e17e3a377929864f43a83a19934e349b049
Kwon Youngbin <kyb951020@naver.com>
2022-01-30 20:04:05 +0900

    비즈니스 로직을 담당하는 서비스 구현 테스트

--- demo/src/main/java/com/example/demo/controller/TodoController.java
+++ demo/src/main/java/com/example/demo/controller/TodoController.java
@@ -1,21 +1,40 @@
 package com.example.demo.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.dto.ResponseDTO;
 import com.example.demo.dto.TodoDTO;
+import com.example.demo.service.TodoService;
 
 @RestController
 @RequestMapping("todo")
 public class TodoController {
+	
+	@Autowired
+	private TodoService service;
+	
 	@GetMapping("/testTodo")
 	public ResponseEntity<?> testTodo(){
 		String id = "777";
 		String title = "SUCCESS";
 		boolean done = true;
 		TodoDTO response = TodoDTO.builder().id(id).title(title).done(done).build();
+		return ResponseEntity.ok().body(response);
+	}
+	
+	@GetMapping("/test")
+	public ResponseEntity<?> testTodoService(){
+		String str = service.testService();
+		List<String> list = new ArrayList<>();
+		list.add(str);
+		ResponseDTO<String> response = ResponseDTO.<String>builder().data(list).build();
 		return ResponseEntity.ok().body(response);
 	}
 }
