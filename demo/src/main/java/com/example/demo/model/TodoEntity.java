902b47c2fb6a2193a7f1834cc70365c1261a00f9
Kwon Youngbin <kyb951020@naver.com>
2022-01-31 15:45:58 +0900

    나만의 Generator를 사용한 기본 키 생성 및 그외 엔티티로 사용할 클래스 설정

--- demo/src/main/java/com/example/demo/model/TodoEntity.java
+++ demo/src/main/java/com/example/demo/model/TodoEntity.java
@@ -1,5 +1,12 @@
 package com.example.demo.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.GenericGenerator;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -9,7 +16,12 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor			// 매개변수가 없는 생성자 구현
 @AllArgsConstructor			// 모든 매개변수를 받는 생성자 구현
 @Data						// Getter, Setter
+@Entity
+@Table(name = "Todo")
 public class TodoEntity {
+	@Id
+	@GeneratedValue(generator="system-uuid")
+	@GenericGenerator(name="system-uuid", strategy = "uuid")
 	private String id;
 	private String userId;
 	private String title;
