3c10dae78a13dd5f859401697a63a06d06419e78
Kwon Youngbin <kyb951020@naver.com>
2022-01-31 15:53:08 +0900

    Todo 애플리케이션을 위한 리포지터리 생성 및 서비스 내에서의 테스트

--- demo/src/main/java/com/example/demo/service/TodoService.java
+++ demo/src/main/java/com/example/demo/service/TodoService.java
@@ -1,10 +1,24 @@
 package com.example.demo.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.example.demo.model.TodoEntity;
+import com.example.demo.persistence.TodoRepository;
 
 @Service
 public class TodoService {
+	
+	@Autowired
+	private TodoRepository repository;
+	
 	public String testService() {
-		return "Test Service";
+		// TodoEntity 생성
+		TodoEntity entity = TodoEntity.builder().title("My first todo item.").build();
+		// TodoEntity 저장
+		repository.save(entity);
+		// TodoEntity 검색
+		TodoEntity savedEntity = repository.findById(entity.getId()).get();
+		return savedEntity.getTitle();
 	}
 }
