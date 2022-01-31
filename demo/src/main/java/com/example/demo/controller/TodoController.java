8b5da1c4e2fb9f8b5e692b820a1658a56256c879
Kwon Youngbin <kyb951020@naver.com>
2022-01-31 17:05:50 +0900

    컨트롤러에 Create 기능 구현

--- demo/src/main/java/com/example/demo/controller/TodoController.java
+++ demo/src/main/java/com/example/demo/controller/TodoController.java
@@ -2,15 +2,19 @@ package com.example.demo.controller;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.dto.ResponseDTO;
 import com.example.demo.dto.TodoDTO;
+import com.example.demo.model.TodoEntity;
 import com.example.demo.service.TodoService;
 
 @RestController
@@ -19,6 +23,39 @@ public class TodoController {
 	
 	@Autowired
 	private TodoService service;
+	
+	@PostMapping
+	public ResponseEntity<?> createTodo(@RequestBody TodoDTO dto){
+		try {
+			String temporaryUserId = "temporary-user";
+			
+			// (1) TodoEntity로 변환
+			TodoEntity entity = TodoDTO.toEntity(dto);
+			
+			// (2) id를 null로 초기화 (생성 당시에는 id가 없어야 하기 때문)
+			entity.setId(null);
+			
+			// (3) 임시 사용자 아이디 설정
+			entity.setUserId(temporaryUserId);
+			
+			// (4) 서비스를 이용해 Todo 엔티티 생성
+			List<TodoEntity> entities = service.create(entity);
+			
+			// (5) 자바 스트림을 이용해 리턴된 엔티티 리스트를 TodoDTO 리스트로 변환
+			List<TodoDTO> dtos = entities.stream().map(TodoDTO::new).collect(Collectors.toList());
+			
+			// (6) 변환된 TodoDTO 리스트를 이용해 ResponseDTO를 초기화한다.
+			ResponseDTO<TodoDTO> response = ResponseDTO.<TodoDTO>builder().data(dtos).build();
+			
+			// (7) ResponseDTO를 리턴한다.
+			return ResponseEntity.ok().body(response);
+		} catch (Exception e) {
+			// (8) 혹시 예외가 있는 경우 dto 대신 error에 메시지를 넣어 리턴한다.
+			String error = e.getMessage();
+			ResponseDTO<TodoDTO> response = ResponseDTO.<TodoDTO>builder().error(error).build();
+			return ResponseEntity.badRequest().body(response);
+		}
+	}
 	
 //	@GetMapping("/testTodo")
 //	public ResponseEntity<?> testTodo(){
